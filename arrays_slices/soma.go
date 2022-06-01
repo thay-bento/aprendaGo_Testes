b75b3a5321aecac8a57c22ad7d68c8cfa99298b0
thay-bento <thayna.silva@stone.com.br>
2022-06-01 20:15:54 -0300

    Arrays e slices updates

--- arrays_slices/soma.go
+++ arrays_slices/soma.go
@@ -10,6 +10,34 @@ func Soma(numeros []int) int {
 	}
 	return soma
 }
+
+func SomaTudo(numerosParaSomar ...[]int) []int {
+	// quantidadeDeNumeros := len(numerosParaSomar)
+	// somas = make([]int, quantidadeDeNumeros)
+
+	// for i, numeros := range numerosParaSomar {
+	// 	somas[i] = Soma(numeros)
+	// }
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		somas = append(somas, Soma(numeros))
+	}
+	return somas
+}
+
+func SomaTodoOResto(numerosParaSomar ...[]int) []int {
+	var somas []int
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+	return somas
+}
+
 func main() {
 
 }
